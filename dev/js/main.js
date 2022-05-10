32db00b56e7d42df2a3bcbbcf1eebc5e5fc703e8
motosasha <moto.sasha@gmail.com>
2022-05-10 14:23:49 +0300

    + edging

---
+++ dev/js/main.js
@@ -0,0 +1,6 @@
+//= ../../node_modules/current-device/umd/current-device.js
+//= ../../node_modules/imask/dist/imask.js
+//= ../../node_modules/swiper/swiper-bundle.js
+//= ../../node_modules/smooth-scroll/dist/smooth-scroll.js
+//= partials/hystmodal.min.js
+//= partials/user.js
