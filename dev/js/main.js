a8513264d379a53aea6e308dfc2ad877900ebf7e
motosasha <moto.sasha@gmail.com>
2022-05-16 21:34:33 +0300

    + sections

--- dev/js/main.js
+++ dev/js/main.js
@@ -1,6 +1,7 @@
 //= ../../node_modules/current-device/umd/current-device.js
 //= ../../node_modules/imask/dist/imask.js
 //= ../../node_modules/swiper/swiper-bundle.js
+//= ../../node_modules/fslightbox/index.js
+//= ../../node_modules/image-compare-viewer/dist/image-compare-viewer.min.js
 //= ../../node_modules/smooth-scroll/dist/smooth-scroll.js
-//= partials/hystmodal.min.js
 //= partials/user.js
--- dev/js/partials/user.js
+++ dev/js/partials/user.js
@@ -26,8 +26,18 @@ window.onload = function() {
 	}
 
 	// modals
-	let modals = new HystModal({
+	const modals = new HystModal({
 		linkAttributeName: 'data-hystmodal',
+		catchFocus: true,
+		closeOnEsc: true,
+		afterClose: function(modal){
+			console.log('Message after modal has closed');
+			console.log(modal);
+			let videoFrame = modal.openedWindow.querySelector('iframe');
+			if(videoFrame){
+				videoFrame.contentWindow.postMessage('{"event":"command","func":"pauseVideo","args":""}', '*');
+			}
+		},
 	});
 	thanks = function () {
 		modals.open('#thanks')
@@ -65,4 +75,159 @@ window.onload = function() {
 		e.preventDefault();
 		window.scrollTo({top: 0, behavior: 'smooth'});
 	})
+
+	// sliders
+	let teamSwiper = new Swiper('.team__slider', {
+		loop: true,
+		navigation: {
+			nextEl: '.team__next',
+			prevEl: '.team__prev',
+		},
+		slidesPerView: 'auto',
+	});
+	let docsSwiper = new Swiper('.docs__slider', {
+		navigation: {
+			nextEl: '.docs__next',
+			prevEl: '.docs__prev',
+		},
+		breakpoints: {
+			992: {
+				slidesPerView: 3,
+				spaceBetween: 32
+			},
+			767: {
+				slidesPerView: 2,
+				spaceBetween: 32
+			},
+		},
+		slidesPerView: 1,
+		spaceBetween: 32
+	});
+	let reviewsSwiper = new Swiper('.reviews__slider', {
+		navigation: {
+			nextEl: '.reviews__next',
+			prevEl: '.reviews__prev',
+		},
+		breakpoints: {
+			992: {
+				slidesPerView: 4,
+				spaceBetween: 32
+			},
+			767: {
+				slidesPerView: 2,
+				spaceBetween: 32
+			},
+		},
+		slidesPerView: 1,
+		spaceBetween: 32
+	});
+	let reviewsVideoSwiper = new Swiper('.reviews__video-slider', {
+		navigation: {
+			nextEl: '.reviews__video-next',
+			prevEl: '.reviews__video-prev',
+		},
+		breakpoints: {
+			767: {
+				slidesPerView: 2,
+				spaceBetween: 32
+			},
+		},
+		slidesPerView: 1,
+		spaceBetween: 32
+	});
+
+	// works
+	let tabLabels = document.querySelectorAll('.works__tabs-head .works__tab');
+	let tabPanes = document.querySelectorAll('.works__tabs-content');
+	function activateTab(e) {
+		e.preventDefault();
+		tabLabels.forEach(function(label, index){
+			label.classList.remove('works__tab_active');
+		});
+		[].forEach.call(tabPanes, function(pane, index){
+			pane.classList.remove('works__tabs-content_active');
+		});
+		e.target.classList.add('works__tab_active');
+		let clickedTab = e.target.getAttribute('data-href');
+		document.querySelector(clickedTab).classList.add('works__tabs-content_active');
+	}
+	tabLabels.forEach(function(label, index){
+		label.addEventListener("click", activateTab);
+	});
+
+	let projectsTabs = document.querySelectorAll('.project-tabs .project-tabs__tab');
+	function theTabClicks(tabClickEvent) {
+		let clickedTab = tabClickEvent.currentTarget;
+		let tabParent = tabClickEvent.currentTarget.parentNode.parentNode.parentNode.parentNode;
+		let projectsTabs = tabParent.querySelectorAll('.project-tabs .project-tabs__tab');
+		for (let i = 0; i < projectsTabs.length; i++) {
+			projectsTabs[i].classList.remove('project-tabs__tab_active');
+		}
+
+		clickedTab.classList.add('project-tabs__tab_active');
+		tabClickEvent.preventDefault();
+		let contentPanes = tabParent.querySelectorAll('.project-tabs__tabs-content');
+		for (let i = 0; i < contentPanes.length; i++) {
+			contentPanes[i].classList.remove('project-tabs__tabs-content_active');
+		}
+		let anchorReference = tabClickEvent.target;
+		let activePaneId = anchorReference.getAttribute('data-href');
+		let activePane = tabParent.querySelector(activePaneId);
+		activePane.classList.add('project-tabs__tabs-content_active');
+	}
+	for (let i = 0; i < projectsTabs.length; i++) {
+		projectsTabs[i].addEventListener("click", theTabClicks)
+	}
+
+	let projectTabs = document.querySelectorAll('.project .project__tab');
+	function theProjectTabClicks(tabClickEvent) {
+		let clickedTab = tabClickEvent.currentTarget;
+		let tabParent = tabClickEvent.currentTarget.parentNode.parentNode.parentNode;
+		let projectTabs = tabParent.querySelectorAll('.project .project__tab');
+		for (let i = 0; i < projectTabs.length; i++) {
+			projectTabs[i].classList.remove('project__tab_active');
+		}
+		console.log(tabParent);
+
+		clickedTab.classList.add('project__tab_active');
+		tabClickEvent.preventDefault();
+		let contentPanes = tabParent.querySelectorAll('.project__item');
+		for (let i = 0; i < contentPanes.length; i++) {
+			contentPanes[i].classList.remove('project__item_active');
+		}
+		let anchorReference = tabClickEvent.target;
+		let activePaneId = anchorReference.getAttribute('data-href');
+		let activePane = tabParent.querySelector(activePaneId);
+		activePane.classList.add('project__item_active');
+	}
+	for (let i = 0; i < projectTabs.length; i++) {
+		projectTabs[i].addEventListener("click", theProjectTabClicks)
+	}
+
+	// before-after slider
+	const viewers = document.querySelectorAll(".image-compare");
+	const options = {
+		controlColor: "#ffffff",
+		controlShadow: false,
+		addCircle: true,
+		addCircleBlur: true,
+
+		showLabels: true,
+		labelOptions: {
+			before: 'До ремонта',
+			after: 'После ремонта',
+			onHover: false
+		},
+
+		smoothing: true,
+		smoothingAmount: 300,
+
+		hoverStart: false,
+		verticalMode: false,
+		startingPoint: 50,
+		fluidMode: false
+	};
+	viewers.forEach((element) => {
+		let view = new ImageCompare(element, options).mount();
+	});
 };
